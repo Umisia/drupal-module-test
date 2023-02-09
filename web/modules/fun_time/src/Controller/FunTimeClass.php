fc47b62cf21e33683e2a654a570be093953177db
umisia <maciaszek89@gmail.com>
2023-02-09 14:07:28 +0000

    2hours up

---
+++ web/modules/fun_time/src/Controller/FunTimeClass.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\fun_time\Controller;
+
+class FunTimeClass {
+public function fun_method() {
+
+    return [
+        '#type' => 'markup',
+        '#markup' => 'text test'
+    ];
+
+    }
+}
+
+
+
+?>
---
+++ web/modules/fun_time/src/Form/ConfigForm.php
@@ -0,0 +1,53 @@
+<?php 
+
+namespace Drupal\fun_time\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ConfigForm extends ConfigFormBase {
+
+    Const CONFIGNAME = "config_form2.settings.yml";
+
+    public function getFormId() {
+        return "config_form_settings";
+    }
+
+    protected function getEditableConfigNAmes() {
+        return [
+            static::CONFIGNAME,
+
+        ];
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state) {
+        $config = $this->config(static::CONFIGNAME);
+        $form['title'] = [
+            '#type' => 'textfield',
+            '#title' => 'Title',
+            '#default_value' => $config->get("title"),
+        ];
+
+        $form['admin_description'] = [
+            '#type' => 'textfield',
+            '#title' => 'Admin description',
+            '#default_value' => $config->get("admin_description"),
+        ];
+
+        return Parent::buildForm($form, $form_state);
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        $config = $this->config(static::CONFIGNAME);
+        $config->set("title", $form_state->getValue("title"));
+        $config->set("admin_description", $form_state->getValue("admin_description"));
+        $config->save();
+
+    }
+
+}
+
+
+
+
+?>
